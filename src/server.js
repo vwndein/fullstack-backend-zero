a8666bcc35daa62d75f72ac19905894c7eb984b5
Luke Pham <vandenn.pham@gmail.com>
2025-10-21 20:42:11 -0700

    connect db sql

--- src/server.js
+++ src/server.js
@@ -6,12 +6,28 @@ const app = express(); // app express
 const port = process.env.PORT || 8081; //port
 const hostname = process.env.HOST_NAME;
 const webRoutes = require("./routes/web.js");
+const mysql = require("mysql2");
 
 //config template engine
 configViewEngine(app);
 
 //khai bao route
 app.use("/", webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "hoidanit",
+});
+
+// simple query
+connection.query("select * from Users u", function (err, results, fields) {
+  console.log(">>>> results=", results);
+  console.log(">>>>fields=", fields);
+});
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
